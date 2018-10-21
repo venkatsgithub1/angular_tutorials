d24308845074dc525512ce0f0a30e92a1aece42d
venkatsgithub1 <venkatsgithub@gmail.com>
2018-10-22 00:05:59 +0530

    initial commit

---
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+// COMMON MODULE CAN BE DELETED AND DECLARATIONS OF
+// NGMODULE CAN BE DELETED SINCE NONE OF THE OTHER
+// COMPONENTS WILL BE ADDED HERE.
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'detail/:id', component: HeroDetailComponent }
+];
+
+@NgModule({
+  exports: [
+    RouterModule
+  ],
+  imports: [
+    RouterModule.forRoot(routes)
+  ]
+})
+export class AppRoutingModule { }
